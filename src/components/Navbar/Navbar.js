3e3846e750efdf8b19ec1659fa737cf4cc787d63
hussain2023 <iamhussainali2@gmail.com>
2022-10-09 15:34:38 +0530

    added navlink with conditonal css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <Link to={"/home"}>Home</Link>
-      <Link to={"/countries"}>Countries</Link>
-      <Link to={"/contact"}>Contact Us</Link>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "font-bold tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400 text-xl ml-12"
+            : "font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400 text-xl ml-12"
+        }
+        to={"/home"}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "font-bold tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400 text-xl ml-12"
+            : "font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400 text-xl ml-12"
+        }
+        to={"/countries"}
+      >
+        Countries
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          isActive
+            ? "font-bold tracking-wide text-blue-700 transition-colors duration-200 hover:text-deep-purple-accent-400 text-xl ml-12"
+            : "font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400 text-xl ml-12"
+        }
+        to={"/contact"}
+      >
+        Contact Us
+      </NavLink>
     </div>
   );
 };
